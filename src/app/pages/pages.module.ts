ea21eb50d3061d7a920d881b2638240460b3bc05
oluciams <oluciams84@gmail.com>
2023-09-28 13:24:47 -0500

    add progress bar in the progress component

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { SharedModule } from '../shared/shared.module';
-import { RouterModule } from '@angular/router';
-
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
@@ -26,8 +26,9 @@ import { PagesComponent } from './pages.component';
   ],
   imports: [
     CommonModule,
+    FormsModule,
     SharedModule,
-    RouterModule
+    RouterModule,
   ],
 })
 export class PagesModule {}
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -3,9 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
-  styles: [
-  ]
+  styleUrls: ['./progress.component.css'],
 })
 export class ProgressComponent {
 
+  progreso: number = 30;
+
+  get getPorcentaje() {
+    return `${ this.progreso }%`;
+  }
+
+  cambiarValor(valor: number) {
+
+    if (this.progreso >= 100 && valor >= 0) {
+      return this.progreso = 100;
+
+    }
+
+    if (this.progreso <= 0 && valor < 0) {
+      return this.progreso = 0;
+    }
+
+    this.progreso = this.progreso + valor
+
+    return
+
+  }
 }
