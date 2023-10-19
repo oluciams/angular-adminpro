988fbb3561fed9d5638d771292c3c9845cec740f
oluciams <oluciams84@gmail.com>
2023-10-19 11:11:28 -0500

    add incrementador component

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,6 +4,8 @@ import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
 import { SharedModule } from '../shared/shared.module';
+import { ComponentsModule } from '../components/components.module';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
@@ -29,6 +31,7 @@ import { PagesComponent } from './pages.component';
     FormsModule,
     SharedModule,
     RouterModule,
+    ComponentsModule,
   ],
 })
 export class PagesModule {}
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -7,26 +7,4 @@ import { Component } from '@angular/core';
 })
 export class ProgressComponent {
 
-  progreso: number = 30;
-
-  get getPorcentaje() {
-    return `${ this.progreso }%`;
   }
-
-  cambiarValor(valor: number) {
-
-    if (this.progreso >= 100 && valor >= 0) {
-      return this.progreso = 100;
-
-    }
-
-    if (this.progreso <= 0 && valor < 0) {
-      return this.progreso = 0;
-    }
-
-    this.progreso = this.progreso + valor
-
-    return
-
-  }
-}
