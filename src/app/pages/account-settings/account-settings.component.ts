9eacaf6eeabc1d7666dba8062ca8dd18cb8f3d87
oluciams <oluciams84@gmail.com>
2023-10-25 19:25:58 -0500

    add account-setting component in pages folder

---
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account-settings',
+  templateUrl: './account-settings.component.html',
+  styles: [
+  ]
+})
+export class AccountSettingsComponent {
+
+}
