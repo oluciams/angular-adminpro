14e177efbd48083d4e237191707cee94f7213ada
oluciams <oluciams84@gmail.com>
2024-02-02 18:03:19 -0500

    chage main CSS dynamically

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-account-settings',
   templateUrl: './account-settings.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class AccountSettingsComponent {
+export class AccountSettingsComponent implements OnInit {
 
+  public linkTheme = document.querySelector('#theme');
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  changeTheme(theme: string) {
+    
+    const url = `./assets/css/colors/${theme}.css`;
+
+    this.linkTheme?.setAttribute('href', url);
+    localStorage.setItem('theme', url);
+  }
 }
