f09fe524b17bb84f163389e5929982f74e438f63
oluciams <oluciams84@gmail.com>
2024-02-06 16:27:55 -0500

    add settings service

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingsService } from '../../services/settings.service';
 
 @Component({
   selector: 'app-account-settings',
@@ -7,40 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountSettingsComponent implements OnInit {
 
-  public linkTheme = document.querySelector('#theme');
-  //public links: NodeListOf<Element>;
-
-  constructor() {}
+  constructor( private settingsService: SettingsService ) {}
 
   ngOnInit(): void {
-    //this.links = document.querySelectorAll('.selector');
-    this.checkCurrentTheme();
+    this.settingsService.checkCurrentTheme();
   }
 
   changeTheme(theme: string) {
-    const url = `./assets/css/colors/${theme}.css`;
-
-    this.linkTheme?.setAttribute('href', url);
-    localStorage.setItem('theme', url);
-
-    this.checkCurrentTheme();
-  }
-
-  checkCurrentTheme() {
-
-    const links = document.querySelectorAll('.selector');
-
-    links.forEach((elem) => {
-      elem.classList.remove('working');
-      const btnTheme = elem.getAttribute('data-theme');
-      const btnThemeUrl = `./assets/css/colors/${btnTheme}.css`;
-      const currentTheme = this.linkTheme?.getAttribute('href');
-
-      if (btnThemeUrl === currentTheme) {
-        elem.classList.add('working');
-      }
-
-    });
+    this.settingsService.changeTheme( theme )
     
   }
+
+  // checkCurrentTheme() {
+
+  //   const links = document.querySelectorAll('.selector');
+
+  //   links.forEach((elem) => {
+  //     elem.classList.remove('working');
+  //     const btnTheme = elem.getAttribute('data-theme');
+  //     const btnThemeUrl = `./assets/css/colors/${btnTheme}.css`;
+  //     const currentTheme = this.linkTheme?.getAttribute('href');
+
+  //     if (btnThemeUrl === currentTheme) {
+  //       elem.classList.add('working');
+  //     }
+
+  //   });
+  // }
 }
--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingsService } from '../services/settings.service';
 
 @Component({
   selector: 'app-pages',
@@ -7,15 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagesComponent implements OnInit {
 
-  public linkTheme = document.querySelector('#theme');
+  constructor( private settingsService: SettingsService) {}
 
-  constructor() {}
-
-  ngOnInit(): void {
-
-    const url = localStorage.getItem('theme') || './assets/css/colors/purple-dark.css';
-
-    this.linkTheme?.setAttribute('href', url);
-
-  }
+  ngOnInit(): void {}
 }
