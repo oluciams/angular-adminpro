cd193c16a65321997dcf0472dfa8677b38133bf3
oluciams <oluciams84@gmail.com>
2024-02-07 14:04:55 -0500

    adds function to use scripts from imported files in the index.html

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SettingsService } from '../services/settings.service';
 
+declare function customInitFunctions(): any;
+
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
@@ -10,5 +12,7 @@ export class PagesComponent implements OnInit {
 
   constructor( private settingsService: SettingsService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    customInitFunctions();
+  }
 }
