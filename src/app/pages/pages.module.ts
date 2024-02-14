9a409a1ed0efe2e578e6c34116b0b8575ef315fb
Olga Marin <oluciams84@gmail.com>
2024-02-14 10:31:21 -0500

    add component promesas

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -11,6 +11,7 @@ import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromesasComponent } from './promesas/promesas.component';
 
 
 
@@ -21,6 +22,7 @@ import { AccountSettingsComponent } from './account-settings/account-settings.co
     Grafica1Component,
     PagesComponent,
     AccountSettingsComponent,
+    PromesasComponent,
   ],
   exports: [
     DashboardComponent,
@@ -28,6 +30,7 @@ import { AccountSettingsComponent } from './account-settings/account-settings.co
     Grafica1Component,
     PagesComponent,
     AccountSettingsComponent,
+    PromesasComponent
   ],
   imports: [
     CommonModule,
--- src/app/services/sidebar.service.ts
+++ src/app/services/sidebar.service.ts
@@ -10,8 +10,9 @@ export class SidebarService {
       icono: 'mdi mdi-gauge',
       submenu: [
         { titulo: 'Main', url: '/' },
-        { titulo: 'ProgressBar', url: 'progress' },
         { titulo: 'Gráficas', url: 'grafica1' },
+        { titulo: 'ProgressBar', url: 'progress' },
+        { titulo: 'Promesas', url: 'promesas' },
       ],
     },
   ];
