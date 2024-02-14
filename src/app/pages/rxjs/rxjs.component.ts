9e64a905f9eefe6e486f606ea7032129c0f0d17c
Olga Marin <oluciams84@gmail.com>
2024-02-14 18:47:38 -0500

    adds filter, take, map operators

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
-import { Observable, retry } from "rxjs";
+import { Observable, interval } from "rxjs";
+import { filter, map, retry, take } from "rxjs/operators";
 
 @Component({
   selector: "app-rxjs",
@@ -8,13 +9,39 @@ import { Observable, retry } from "rxjs";
 })
 export class RxjsComponent {
   constructor() {
+    // this.retornaObservable().pipe(
+    //   retry(1)
+    // ).subscribe(
+    //   valor => console.log("Subs:", valor),
+    //   error => console.warn('Error:', error),
+    //   () => console.info('Obs terminado')
+    // );
 
+    this.retornaIntervalo().subscribe(console.log);
+  }
+
+  retornaIntervalo(): Observable<number> {
+    // const intervalo$ = interval(2000)
+    //                     .pipe(
+    //                       take(4),
+    //                       map(valor => valor + 1 )
+    //                     );
+
+    // return intervalo$;
+
+    return interval(2000).pipe(
+      map((valor) => valor + 1),
+      filter(valor => valor % 2 === 0 ? true : false),
+      take(10),
+    );
+  }
+
+  retornaObservable(): Observable<number> {
     let i = -1;
 
-    const obs$ = new Observable((observer) => {
+    return new Observable<number>((observer) => {
       const intervalo = setInterval(() => {
-
-        i++
+        i++;
         observer.next(i);
 
         if (i === 4) {
@@ -22,21 +49,10 @@ export class RxjsComponent {
           observer.complete();
         }
 
-        if (i === 2) { 
-          i = 0;
-          observer.error('i llego al valor de 2');
+        if (i === 2) {
+          observer.error("i llego al valor de 2");
         }
-
       }, 2000);
     });
-
-    obs$.pipe(
-      retry(1)
-
-    ).subscribe(
-      valor => console.log("Subs:", valor),
-      error => console.warn('Error:', error),
-      () => console.info('Obs terminado')
-    );
   }
 }
