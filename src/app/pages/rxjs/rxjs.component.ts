a1bfd266eb4241e5098d50c3670ffb69dad5bb15
Olga Marin <oluciams84@gmail.com>
2024-02-14 12:26:13 -0500

    add observable manually

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-rxjs',
-  templateUrl: './rxjs.component.html',
-  styles: [
-  ]
+  selector: "app-rxjs",
+  templateUrl: "./rxjs.component.html",
+  styles: [],
 })
 export class RxjsComponent {
+  constructor() {
+    const obs$ = new Observable((observer) => {
+      let i = 0;
+      const intervalo = setInterval(() => {
 
+        i++
+        observer.next(i);
+
+        if (i === 4) {
+          clearInterval(intervalo);
+          observer.complete();          
+        }
+
+         if (i === 2) {           
+           observer.error('i llego al valor de 2');
+         }
+
+      }, 2000);
+    });
+
+    obs$.subscribe(
+      valor => console.log("Subs:", valor),
+      error => console.warn('Error:', error),
+      () => console.info('Obs terminado')
+    );
+  }
 }
